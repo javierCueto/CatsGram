a246c620b2ad6e86d1dad3cfb9b5c6803a83cf16
Javier <javelie@outlook.com>
2020-04-16 21:34:53 -0600

    save data in firebase

--- CatsGram/UploadViewController.swift
+++ CatsGram/UploadViewController.swift
@@ -7,24 +7,92 @@
 //
 
 import UIKit
+import Firebase
 
-class UploadViewController: UIViewController {
-
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var commentText: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func chooseImage(){
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        
+        present(pickerController, animated: true, completion: nil)
+        
     }
-    */
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButttonClicked(_ sender: Any) {
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            imageReference.putData(data, metadata: nil) { (metadata, error) in
+                if let safeError = error {
+                    self.makeAlert(titleInput: "Error", messageInput: safeError.localizedDescription)
+                    
+                }else {
+                    imageReference.downloadURL { (url, error) in
+                        if let safeError = error {
+                            print(safeError.localizedDescription)
+                        }else{
+                            let imageUrl = url?.absoluteString
+                            
+                            let firesoreDatabase = Firestore.firestore()
+                            
+                            var firestoreReference : DocumentReference? = nil
+                            
+                            let firestorePost = ["imageUrl" : imageUrl!, "postedBy" : Auth.auth().currentUser!.email!, "postComment" : self.commentText.text!, "date" : "date", "like" : 0] as [String : Any]
+                            
+                            firestoreReference = firesoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { (error) in
+                                if  let safeError =  error  {
+                                    self.makeAlert(titleInput: "Error", messageInput: safeError.localizedDescription)
+                                }else{
+                                    self.makeAlert(titleInput: "Succes", messageInput: "All data are saved")
+                                }
+                            })
+                            
+                            
+                        }
+                    }
+                }
+                
+            }
+        }
+        
+    }
+    
+    
+    
+    func makeAlert(titleInput: String, messageInput: String){
+           let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert)
+           let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+           alert.addAction(okButton)
+           self.present(alert, animated: true, completion: nil)
+          // indicatorActivity.isHidden = true
+       }
+    
 }
